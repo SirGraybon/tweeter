db1cddab48adc7a7bfa54c61af44836b8621a140
SirGraybon <graydonritchie@gmail.com>
2023-04-25 23:21:40 -0400

    implemented dummy tweets

--- public/scripts/feed.js
+++ public/scripts/feed.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
 
-$(".feed").hover(function() {
+  $(".feed").hover(function(event) {
+    event.stopImmediatePropagation();
+    $(this).css("box-shadow", "5px 5px 9px rgba(94,104,121, 1").css("margin", "0 5px 5px 0");
+  }, function() {
 
-  $(".feed").css("box-shadow", "5px 5px 9px rgba(94,104,121, 1").css("margin", "0 5px 5px 0")
-}, function() {
+    $(this).css("box-shadow", "5px 5px 9px rgba(94,104,121, 0)").css("margin", " 5px 0 0 5px");
+  });
 
-  $(".feed").css("box-shadow", "5px 5px 9px rgba(94,104,121, 0)").css("margin", " 5px 0 0 5px");
+  $("i").hover(function() {
+    $(this).css("color", "#ae8a44");
+  }, function() { $(this).css("color", "#545149")
 });
 
 });
